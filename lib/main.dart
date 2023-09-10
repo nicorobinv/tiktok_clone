ebcfd99de7e2e37b297188d0170d911fb3e94fdb
seungwon jang <43410466+nicorobinv@users.noreply.github.com>
2023-09-10 21:50:43 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+
+void main() {
+  runApp(const TictokApp());
+}
+
+class TictokApp extends StatelessWidget {
+  const TictokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tiktok Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        useMaterial3: true,
+      ),
+      home: const SignUpScreen(),
+    );
+  }
+}
