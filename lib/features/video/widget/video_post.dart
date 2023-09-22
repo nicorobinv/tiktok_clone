3c4ae8eae7c5cffd12974977a0f4e8622402dd89
seungwon jang <43410466+nicorobinv@users.noreply.github.com>
2023-09-22 19:50:27 +0900

    fin video timeline

--- lib/features/video/widget/video_post.dart
+++ lib/features/video/widget/video_post.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/video/widget/video_button.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class Videopost extends StatefulWidget {
   final Function onVideoFinished;
-  const Videopost({super.key, required this.onVideoFinished});
+
+  final int index;
+
+  const Videopost(
+      {super.key, required this.onVideoFinished, required this.index});
 
   @override
   State<Videopost> createState() => _VideopostState();
 }
 
-class _VideopostState extends State<Videopost> {
-  final VideoPlayerController _videoPlayerController =
-      VideoPlayerController.asset("assets/videos/IMG_3079.MOV");
+class _VideopostState extends State<Videopost>
+    with SingleTickerProviderStateMixin {
+  late final VideoPlayerController _videoPlayerController;
+
+  final Duration _animationDuration = const Duration(milliseconds: 200);
+
+  late final AnimationController _animationController;
+
+  bool _isPaused = false;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -23,17 +38,26 @@ class _VideopostState extends State<Videopost> {
   }
 
   void _initVideoPlayer() async {
+    _videoPlayerController =
+        VideoPlayerController.asset("assets/videos/IMG_3079.MOV");
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
-    setState(() {
-      _videoPlayerController.addListener(_onVideoChange);
-    });
+    await _videoPlayerController.setLooping(true);
+    _videoPlayerController.addListener(_onVideoChange);
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
   }
 
   @override
@@ -42,18 +66,128 @@ class _VideopostState extends State<Videopost> {
     super.dispose();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void __onTogglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      _animationController.reverse();
+    } else {
+      _videoPlayerController.play();
+      _animationController.forward();
+    }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: _videoPlayerController.value.isInitialized
-              ? VideoPlayer(_videoPlayerController)
-              : Container(
-                  color: Colors.black,
+    return VisibilityDetector(
+      key: Key("${widget.index}"),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: __onTogglePause,
+            ),
+          ),
+          Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child,
+                    );
+                  },
+                  child: AnimatedOpacity(
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animationDuration,
+                    child: const FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
-        ),
-      ],
+              ),
+            ),
+          ),
+          const Positioned(
+            bottom: 20,
+            left: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "@나삐",
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v10,
+                Text(
+                  "大森駅近くの公園",
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Positioned(
+              bottom: 20,
+              right: 10,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    foregroundImage: NetworkImage(
+                      "https://pds.exblog.jp/pds/1/201802/13/19/a0145819_20215598.jpg",
+                    ),
+                    child: Text(
+                      "승삐",
+                    ),
+                  ),
+                  Gaps.v24,
+                  VideoButton(
+                    icon: FontAwesomeIcons.solidHeart,
+                    text: "2.9M",
+                  ),
+                  Gaps.v24,
+                  VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: "33k",
+                  ),
+                  Gaps.v24,
+                  VideoButton(
+                    icon: FontAwesomeIcons.share,
+                    text: "Share",
+                  ),
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
